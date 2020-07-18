346a80736b54b9140cc63025667422764087b275
Timothy Dunphy <Timothy.Dunphy@synchronoss.com>
2020-07-18 13:36:54 -0400

    Added drop database to ec2 mongo.

--- aws_scripts/python/aws_tools/ec2_mongo.py
+++ aws_scripts/python/aws_tools/ec2_mongo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 # Import modules
 import time
 import pymongo
@@ -105,9 +107,75 @@ def create_mongodb(mydict):
         if newdb in dblist:
             print("The database exists.")
         else:
-            mydb = myclient[newdb]
-            collection = mydb['test-collection']
-            x = collection.insert_one(mydict)
+            try:
+                mydb = myclient[newdb]
+                collection = mydb['test-collection']
+                x = collection.insert_one(mydict)
+            except Exception as e:
+                print(Fore.RED + f"Database not created: {newdb} due to: {e}")
+                main()
+
+    print("The current list of database names is:")
+    if myclient != None:
+        # the list_database_names() method returns a list of strings
+        database_names = myclient.list_database_names()
+        counter = 1
+        for db in database_names:
+            message = str(counter) + '. ' + db
+            print(message)
+            counter = counter + 1
+        print ("There are", len(database_names), "databases.")
+
+def drop_mongodb():
+    myclient = connect_db()
+    message = "* Drop MongoDB Database *"
+    banner(message, "*")
+    # get all of the database names
+    db_names_before_drop = myclient.list_database_names()
+    print ("db count BEFORE drop:", len(db_names_before_drop))
+    message = "Select Database"
+    banner(message)
+    print(Fore.CYAN + "Available MongoDB Databases:")
+    if myclient != None:
+        # the list_database_names() method returns a list of strings
+        database_names = myclient.list_database_names()
+        counter = 1
+        for db in database_names:
+            message = str(counter) + '. ' + db
+            print(message)
+            counter = counter + 1
+    print ("There are", len(database_names), "databases.\n")
+    print(f"Please select a database. Enter a number 1 through {len(database_names)}.")
+    choice = input("Enter a number: ")
+    if is_digit(choice) == True:
+        if int(choice) > counter:
+            print("Wrong selection.")
+            set_db()
+        choice = int(choice)
+        choice = choice - 1
+        dropdb = myclient[database_names[choice]]
+        dropdb_name = database_names[choice]
+        print(f"You've selected: {database_names[choice]}\n")
+    else:
+        print("Must enter a digit. Try again.\n")
+    try:
+        db = myclient.newdb
+    except Exception as e:
+        print(f"A pymongo error has occurred: {e}")
+
+    # check the collection names on the database
+    col_list = db.list_collection_names()
+    print ("collections on the unwanted db:", col_list)
+
+    # call MongoDB client object's drop_database() method to delete a db
+    myclient.drop_database(dropdb) # pass db name as string
+
+    # get all of the database names
+    db_names_after_drop = myclient.list_database_names()
+    print ("db count AFTER drop:", len(db_names_before_drop))
+
+    diff = len(db_names_before_drop) - len(db_names_after_drop)
+    print ("difference:", diff)
 
 def insert_doc(mydict):
     mydb, mydb_name, instance_col = set_db()
@@ -152,13 +220,15 @@ def print_db_names():
     myclient = connect_db()
     message = f"* Print DB Names *"
     banner(message, border='*')
-    print(f"This command prints the database names.\n")
     print("The database names are:")
     if myclient != None:
         # the list_database_names() method returns a list of strings
         database_names = myclient.list_database_names()
+        counter = 1
         for db in database_names:
-            print(db)
+            message = str(counter) + '. ' + db
+            print(message)
+            counter = counter + 1
         print ("There are", len(database_names), "databases.")
 
 def print_collections():
@@ -182,13 +252,14 @@ def menu():
     message = "Main Menu"
     banner(message)
     print(Fore.CYAN + "Your available actions: ")
-    print("1. Create a MongoDB Database")
-    print("2. Do a test insert to the DB")
-    print("3. Clear the DB")
-    print("4. Print the DB")
-    print("5. Print DB Names")
-    print("6. Print collections")
-    print("7. Exit ec2 mongo")
+    print("1. Create new MongoDB Database")
+    print("2. Drop MongoDB Database")
+    print("3. Do a test insert to the DB")
+    print("4. Clear the DB")
+    print("5. Print the DB")
+    print("6. Print DB Names")
+    print("7. Print collections")
+    print("8. Exit ec2 mongo")
     print("\n")
 
 
@@ -201,28 +272,31 @@ def main():
     if option == '1':
         create_mongodb(mydict)
         main()
+    elif option == '2':
+        drop_mongodb()
+        main()
     # 1. Do a test insert to the DB
-    elif option  == '2':
+    elif option  == '3':
         x = insert_doc(mydict)
         main()
     # 2. Clear the DB"
-    elif option == '3':
+    elif option == '4':
         clear_db()
         main()
     # 3. Print the DB
-    elif option == '4':
+    elif option == '5':
         mongo_select_all()
         main()
     # 4. Print DB Names
-    elif option == '5':
+    elif option == '6':
        print_db_names()
        main()
     # 5. Print collections
-    elif option == '6':
+    elif option == '7':
        print_collections()
        main()
     # 6. Exit ec2 mongo
-    elif option == '7':
+    elif option == '8':
         exit_program()
     else:
         message = "That is not a valid option."
