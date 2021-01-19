ea4a8ebf4a30733f301b5553d58a2450d6c917ee
Timothy Dunphy <bluethundr@gmail.com>
2021-01-19 15:10:06 -0500

    Put some more mongo client checks into ec2 mongo.

--- aws_scripts/python/aws_tools/ec2_mongo.py
+++ aws_scripts/python/aws_tools/ec2_mongo.py
@@ -504,17 +504,28 @@ def print_reports(interactive,aws_account,aws_account_number):
     myclient = connect_db()
     if myclient != None:
         mydb = myclient["aws_inventories"]
-        try:
-            insert_coll = "ec2_list_" + inputDate
-            collection_names = mydb.list_collection_names()
-            if insert_coll not in collection_names:
-                print(f"Collection name: {insert_coll} does not exist in DB. Try again!")
-                print_reports(interactive,aws_account,aws_account_number)
-            else:
-                insert_coll = mydb[insert_coll]
-        except Exception as e:
-            print(f"An error has occurred: {e}")
-    mongo_export_to_file(interactive, aws_account, aws_account_number,insert_coll,date=inputDate)
+        instance_col = "ec2_list_" + inputDate
+        instance_col = mydb[instance_col]
+    mongo_export_to_file(interactive, aws_account, aws_account_number,instance_col,date=inputDate)
+
+# Choice 11. Exit ec2 Mongo
+
+def menu():
+    message = "Main Menu"
+    banner(message)
+    print(Fore.CYAN + "Your available actions: ")
+    print("1. Create new MongoDB Database")
+    print("2. Drop MongoDB Database")
+    print("3. Do a test insert to the DB")
+    print("4. Clear the DB")
+    print("5. Remove accounts from the DB.")
+    print("6. Print the DB")
+    print("7. Print DB Names")
+    print("8. Print collections")
+    print("9. Export MongoDB to file")
+    print("10. Print Reports")
+    print("11. Exit ec2 mongo")
+    print("\n")
 
 ### Main Function
 def main():
