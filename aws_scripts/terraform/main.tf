6260dd96ca3904be164791091e1d5c0dcb59bbef
Tim Dunphy <bluethundr@gmail.com>
2021-02-10 14:06:51 -0500

    Updated and fixed ssh key issue on aws terraform.

--- aws_scripts/terraform/main.tf
+++ aws_scripts/terraform/main.tf
@@ -8,20 +8,20 @@ terraform {
 }
 
 provider "aws" {
-  profile = "jf-master-pd-gov"
+  profile = "jf-master-pd"
   region  = var.region
 }
 
 resource "aws_instance" "example" {
-  ami                    = var.amis["us-gov-west-1"]
+  ami                    = var.amis["us-east-1"]
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-7f051404"]
-  subnet_id              = "subnet-4dad6304"
-  key_name               = "jf-timd-gov-keypair"
+  vpc_security_group_ids = ["sg-0333d9eaaeb3ab1b0"]
+  subnet_id              = "subnet-3ab1835d"
+  key_name               = "jf-timd-keypair"
 }
 
-resource "aws_key_pair" "jf-timd-gov-keypair" {
-  key_name   = "jf-timd-gov-keypair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvh1RdiRPJBeJzln/uoxbFQAUJXnbBXzmmw1VZMs4WVSH8FaKsFt3eoakhid5nCdY7EwdT7PE0H6ACyELu2GMYHZ3pJXrbyNG196XK4QUT8Cj7/iG6Onwj3esjT90BaBuSB/q1s9D6NzVumJc1WDKVUkF6JSe1mSK9tECgCc9Vu9YOg69/mZ63VJsTKlaLrjndtRowT1F8dWeNCU3pQGHxdfdesFwopWydEmfwuj0yzXw5HebJZWb0ju8ajdeW6bsyvYHnr5Vi0hJ9Hily74u7H6BSU25jjKOWND/v6jDY/nyyBdPvEsN3PEeiIrejnVWIeqKvhzmBWE7bdcFspIJvw== dunphy@BAM-025715-TD.local tdunphy@gmail.com"
+resource "aws_key_pair" "jf-timd-keypair" {
+  key_name   = "jf-timd-keypair"
+  public_key = file("C:\\Users\\bluet\\.ssh\\id_rsa.pub")
 }
 
--- aws_scripts/terraform/variables.tf
+++ aws_scripts/terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default = "us-gov-west-1"
+  default = "us-east-1"
 }
 
 variable "amis" {
